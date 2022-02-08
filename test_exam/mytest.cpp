7396d0f3c8fdd9f9d140dc0ca53e958355038f2f
roccoasc1 <roccoasc1@gmail.com>
2022-02-08 01:19:18 +0100

    Iterators: unique_ptr (second try)

---
+++ test_exam/mytest.cpp
@@ -0,0 +1,97 @@
+#include "stack_pool1.hpp"
+#include<iostream>
+#include <cassert>
+#include <algorithm> 
+
+template <typename T>
+void f(T x);
+
+int main(){
+    /*{
+    stack_pool<int, std::size_t> pool{};
+    auto l = pool.new_stack();
+    
+    pool.print_pool();
+    std::cout << "l == pool.end()    \t" << (l == pool.end()) << "\n"
+              << "l == std::size_t(0)\t" << (l == std::size_t(0)) << std::endl;
+    
+    l = pool.push(42,l);
+    std::cout << "l == std::size_t(1)\t" << (l == std::size_t(1)) << std::endl;
+    pool.print_pool();
+
+    pool.value(l) = 77;
+    pool.print_pool();
+
+
+
+    stack_pool<int, std::size_t> pool1{};
+    auto l1 = pool1.new_stack();
+    l1 = pool1.push(10,l1); // l1 == std::size_t(1)
+    std::cout << "l1 == std::size_t(1)\t" << (l1 == std::size_t(1)) << std::endl; 
+    pool1.print_pool();
+    
+    l1 = pool1.push(11,l1); // l1 == std::size_t(2) <-- later, this node will be deleted
+    std::cout << "l1 == std::size_t(2)\t" << (l1 == std::size_t(2)) << std::endl;
+    pool1.print_pool();
+
+    auto l2 = pool1.new_stack();
+    l2 = pool1.push(20,l2); // l2 == std::size_t(3)
+    std::cout << "l2 == std::size_t(1)\t" << (l2 == std::size_t(3)) << std::endl;
+    pool1.print_pool();
+  
+    l1 = pool1.pop(l1); // that node is deleted, so it is added to free_nodes
+    pool1.print_pool();
+    
+    l2 = pool1.push(21,l2); // l2 == std::size_t(2)
+    std::cout << "l2 == std::size_t(2)\t" << (l2 == std::size_t(2)) << std::endl;
+    pool1.print_pool();
+    }*/
+    {
+    stack_pool<int> pool{22};
+    auto l1 = pool.new_stack();
+    // credits: pi as random number generator :)
+    l1 = pool.push(3, l1);
+    l1 = pool.push(1, l1);
+    l1 = pool.push(4, l1);
+    l1 = pool.push(1, l1);
+    l1 = pool.push(5, l1);
+    l1 = pool.push(9, l1);
+    l1 = pool.push(2, l1);
+    l1 = pool.push(6, l1);
+    l1 = pool.push(5, l1);
+    l1 = pool.push(3, l1);
+    l1 = pool.push(5, l1);
+
+    auto l2 = pool.new_stack();
+    l2 = pool.push(8, l2);
+    l2 = pool.push(9, l2);
+    l2 = pool.push(7, l2);
+    l2 = pool.push(9, l2);
+    l2 = pool.push(3, l2);
+    l2 = pool.push(1, l2);
+    l2 = pool.push(1, l2);
+    l2 = pool.push(5, l2);
+    l2 = pool.push(9, l2);
+    l2 = pool.push(9, l2);
+    l2 = pool.push(7, l2);
+	
+	auto it = pool.begin(l1);
+	auto stop = pool.end(l1);
+	std::cout << (it!=stop) << std::endl;
+	it.print();
+	stop.print();
+	std::cout << *it << std::endl;
+	std::cout << *stop << std::endl;
+	
+	for(;it!=stop;++it)
+		std::cout << (*it) << std::endl;
+    
+    //auto M = std::max_element(pool.begin(l1), pool.end(l1));
+    //std::cout << *M << " == 9?: "<< (*M==9) << "\n"; 
+
+    //auto m = std::min_element(pool.begin(l2), pool.end(l2));
+    //std::cout << *m << " ==1? " << (*m == 1) << std::endl;
+	
+    }
+    return 0; 
+}
---
+++ test_exam/stack_pool.hpp
@@ -0,0 +1,297 @@
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <memory>
+/*
+template<typename node, typename T>
+class _iterator{ //forward iterator
+    node* current;
+public:
+    using value_type = T;
+    using reference = value_type&;
+    using pointer = value_type*;
+    using difference_type = std::ptrdiff_t;
+    using iterator_category = std::forward_iterator_tag;
+    
+    explicit _iterator(node x): current{&x} {}
+    
+    reference operator*() const { return current->value; }
+    
+    pointer operator->(){ return &**this; }
+
+    _iterator& operator++(){
+       // current = reinterpret_cast<node*>(current->value);
+        current = reinterpret_cast<node*>(&current->value);
+        return *this;
+    }
+    
+    _iterator& operator++(int){
+        auto tmp = *this;
+        ++(*this);
+        return tmp;   
+    }
+
+    friend bool operator==(const _iterator& x, const _iterator& y){
+        return x.current == y.current;
+    }
+
+    friend bool operator!=(const _iterator& x, const _iterator& y){
+        return !(x==y);
+    }
+};        
+
+template<typename N, typename T>
+class _iterator{ //forward iterator
+     N current;
+public:
+    using value_type = T;
+    using reference = value_type&;
+    using pointer = value_type*;
+    using difference_type = std::ptrdiff_t;
+    using iterator_category = std::forward_iterator_tag;
+
+    explicit _iterator(N x): current{x} {}
+
+    reference operator*() const { return pool[current-1].value; }
+
+    pointer operator->(){ return &**this; }
+
+    _iterator& operator++(){
+        current = pool[current-1].next;
+        return *this;
+    }
+
+    _iterator& operator++(int){
+        auto tmp = *this;
+        ++(*this);
+        return tmp;
+    }
+
+    friend bool operator==(const _iterator& x, const _iterator& y){
+        return x.current == y.current;
+    }
+
+    friend bool operator!=(const _iterator& x, const _iterator& y){
+        return !(x==y);
+    }
+};*/
+
+
+
+template <typename T, typename N = std::size_t>
+class stack_pool{
+	struct node_t{
+    	T value;
+    	N next;
+ 
+        //explicit node_t(const T& x): value{x} {}
+        //node_t(const T& x, N y): value{x}, next{y}{}        
+        //explicit node_t(T&& x): value{std::move(x)} {}
+        //node_t(T&& x, N y): value{std::move(x)}, next{y} {}
+        //node_t (T val, N nex): value{val}, next{nex} {}
+        //node_t (N n): node_t{node(n)} {}
+        //node_t(node_t&&) = default;
+    };
+ 
+    std::vector<node_t> pool;
+    using stack_type = N;
+	using value_type = T;
+	using size_type = typename std::vector<node_t>::size_type; 				// size_type come size_type di std::vectors
+    stack_type free_nodes = end(); // at the beginning, it is empty			     
+    
+    node_t& node(stack_type x) noexcept { return pool[x-1]; }
+    const node_t& node(stack_type x) const noexcept { return pool[x-1]; }
+
+    template<typename V>
+    stack_type _push(V&& val, stack_type head){
+        if (!empty(free_nodes)){
+            auto new_head = free_nodes;
+            free_nodes = node(free_nodes).next;
+            node(new_head) = node_t{std::forward<V>(val), head}; // move assignement + ctor
+            return new_head;
+        }
+        pool.push_back(node_t{std::forward<V>(val),head}); // con push_back va in fondo
+        return stack_type(pool.size());
+    }
+    
+    public:
+    stack_pool() = default;
+    explicit stack_pool(size_type n) :pool{} { reserve(n); }// reserve n nodes in the pool
+    //stack_pool(const stack_pool& ) = default;
+    //stack_pool(stack_pool&& ) = default;
+    //stack_pool& operator=(stack_pool&& ) = default;
+    //stack_pool& operator=(const stack_pool& ) = default;
+    
+    template <typename O>
+    class _iterator;
+
+    template <typename O> 
+    friend class _iterator;
+
+    template <typename O>
+    class _iterator {
+        stack_type current;
+        public:
+        using value_type = O;
+        using reference = value_type&;
+        using pointer = value_type*;
+        using difference_type = std::ptrdiff_t;
+        using iterator_category = std::forward_iterator_tag;
+         
+        _iterator(stack_type x): current{x} {}
+        //_iterator(const _iterator& x): current{x.current}, sp{x.sp}{}
+        //_iterator(_iterator&& x ): current{std::move(x.current)}, sp{x.sp}{}
+        //_iterator() = default;
+        //_iterator(_iterator&& ) = default;
+        //_iterator(const _iterator& ) = default;
+        //_iterator& operator=(_iterator&& )=default;
+        //_iterator& operator=(const _iterator& ) = default;
+        //~_iterator() noexcept = default;
+        reference operator*() { return value(current); } // const missing
+        
+        pointer operator->(){ return &**this; }
+    
+        _iterator& operator++(){
+            current = next(current);
+            return *this;
+        }
+        
+        _iterator& operator++(int){
+            auto tmp = *this;
+            ++(*this);
+            return tmp;   
+        }
+     
+        friend bool operator==(const _iterator& x, const _iterator& y){ return x.current == y.current; }
+        friend bool operator!=(const _iterator& x, const _iterator& y){ return !(x==y); }
+    };        
+
+    using iterator = _iterator<T>;
+    using const_iterator = _iterator<const T>;
+
+    iterator begin(stack_type x) { return iterator(x); }
+    iterator end(stack_type ) { return iterator(end()); } // this is not a typo
+
+    //const_iterator begin(stack_type x) const { return const_iterator(x, pool); }
+    //const_iterator end(stack_type ) const{ return const_iterator(end(), pool); }
+      
+    //const_iterator cbegin(stack_type x) const { return const_iterator(x, pool); }
+    //const_iterator cend(stack_type ) const { return const_iterator(end(),pool); }
+
+    stack_type new_stack() { return end(); } // return an empty stack
+
+    void reserve(size_type n){ pool.reserve(n); } // reserve n nodes in the pool
+    size_type capacity() const noexcept { return pool.capacity(); } // the capacity of the pool
+
+    bool empty(stack_type x) const noexcept { return x == end(); } // sì se vuoto
+
+    stack_type end() const noexcept { return stack_type(0); }
+
+    T& value(stack_type x){ return node(x).value; }
+    const T& value(stack_type x) const { return node(x).value; }
+
+    stack_type& next(stack_type x) { return node(x).next; }
+    const stack_type& next(stack_type x) const { return node(x).next; }
+
+    stack_type push(const T& val, stack_type head){ return _push(val, head); }
+    stack_type push(T&& val, stack_type head){ return _push(std::move(val), head); }
+/*
+    stack_type push(const T& val, stack_type head){ // meglio implementare fuori forse perché sono grandi
+        if (!empty(free_nodes)){
+            // liberiamo free_nodes
+            auto new_head = free_nodes;		
+            free_nodes = node(free_nodes).next;	
+            // push nel nuovo stack	
+            node(new_head) = node_t{val,head}; // move assignement + ctor
+            return new_head;
+        }
+        else {
+            pool.push_back(node_t{val,head}); // con push_back va in fondo
+            return stack_type(pool.size());
+        }
+    }
+   stack_type push(T&& val, stack_type head){ // std::move somewhere?
+        if (!empty(free_nodes)){
+            // liberiamo free_nodes
+            auto new_head = free_nodes;		
+            free_nodes = node(free_nodes).next;	
+            // push nel nuovo stack	
+            node(new_head) = node_t{val,head}; // move assignement + ctor
+            // node(new_head) = node_t{std::move(val),std::move(head)};
+            //  		node(new_head).next = head;
+            //  		node(new_head).value = val;
+        return new_head;
+        }
+        else {
+            pool.push_back(node_t{val,head}); 
+            // pool.push_back(node_t{std::move(val),std::move(head)});
+            return stack_type(pool.size());
+        }
+    }
+*/
+    stack_type pop(stack_type x){ // delete first node
+        auto new_head = node(x).next;
+        node(x).next = free_nodes;
+        free_nodes = x;
+        return new_head;  
+    }
+
+    stack_type free_stack(stack_type x){ // free entire stack
+        while (!empty(x))
+            x = pop(x);
+        return x;
+    }
+
+    void print_pool() const noexcept {
+        std::cout << "value\tnext\n";
+        for (const auto x : pool)
+            std::cout << x.value << "\t" << x.next << std::endl;
+    }
+    
+  
+    friend node_t& operator++(node_t& x){ return node(x.next); }
+};
+
+/*
+template <typename T, typename N>
+template <typename O>
+class stack_pool<T,N>::_iterator{
+    using index = typename stack_pool<T,N>::stack_type;
+    index current;
+    public:
+    using value_type = O;
+    using reference = value_type&;
+    using pointer = value_type*;
+    using difference_type = index;
+    using iterator_category = std::forward_iterator_tag;
+    
+    explicit _iterator(index x): current{x} {}
+    
+    reference operator*() const {    
+        return value(current);
+     }
+    
+    pointer operator->(){ return &**this; }
+
+    _iterator& operator++(){
+        current = next(current);
+        return *this;
+    }
+    
+    _iterator& operator++(int){
+        auto tmp = *this;
+        ++(*this);
+        return tmp;   
+    }
+
+    friend bool operator==(const _iterator& x, const _iterator& y){
+        return x.current == y.current;
+    }
+
+    friend bool operator!=(const _iterator& x, const _iterator& y){
+        return !(x==y);
+    }
+};        
+
+*/
+
